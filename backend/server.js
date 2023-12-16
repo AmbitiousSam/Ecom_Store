ada6cc9b7702c655830113ef755233929b45b808
anil7620 <anil76201reddy@gmail.com>
2023-12-15 21:08:37 -0600

    getbyid

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import { get } from "http";
+import { getUserById } from "./controllers/userController.js";
 
 const port = process.env.PORT || 5000;
 connectDB();
@@ -20,6 +22,8 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
+// get user by id
+app.get("/api/users/:id", getUserById);
 
 app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
